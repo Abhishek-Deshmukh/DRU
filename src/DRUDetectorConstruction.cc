1ef19d6c7ae5fc07b577f5f2dd1ae691687e3cae
abhishek-deshmukh <deshmukh.abhishek369@gmail.com>
2021-05-24 10:31:41 +0530

    colored the detector

--- src/DRUDetectorConstruction.cc
+++ src/DRUDetectorConstruction.cc
@@ -27,10 +27,12 @@
 /// \file DRUDetectorConstruction.cc
 /// \brief Implementation of the DRUDetectorConstruction class
 
+#include <G4Colour.hh>
 #include "DRUDetectorConstruction.hh"
 
 #include "G4RunManager.hh"
 #include "G4NistManager.hh"
+#include "G4VisAttributes.hh"
 #include "G4Box.hh"
 #include "G4Cons.hh"
 #include "G4Orb.hh"
@@ -39,31 +41,36 @@
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+#include "CADMesh.hh"
 
 DRUDetectorConstruction::DRUDetectorConstruction()
 : G4VUserDetectorConstruction(),
-  fScoringVolume(0)
+  fScoringVolume(nullptr)
 { }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 DRUDetectorConstruction::~DRUDetectorConstruction()
-{ }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+= default;
 
 G4VPhysicalVolume* DRUDetectorConstruction::Construct()
 {  
   // Get nist material manager
   G4NistManager* nist = G4NistManager::Instance();
-  
-  // Envelope parameters
-  //
-  G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
-  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
-   
+    G4Colour  white   (1.0, 1.0, 1.0) ;  // white
+    G4Colour  grey    (0.5, 0.5, 0.5) ;  // grey
+    G4Colour  black   (0.0, 0.0, 0.0) ;  // black
+    G4Colour  red     (1.0, 0.0, 0.0) ;  // red
+    G4Colour  green   (0.0, 1.0, 0.0) ;  // green
+    G4Colour  blue    (0.0, 0.0, 1.0) ;  // blue
+    G4Colour  cyan    (0.0, 1.0, 1.0) ;  // cyan
+    G4Colour  magenta (1.0, 0.0, 1.0) ;  // magenta
+    G4Colour  yellow  (1.0, 1.0, 0.0) ;  // yellow
+  // ENVELOPE
+  // specifications
+  G4double env_sizeXY = 0.2*m;
+  G4double env_sizeZ = 0.2*m;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+
   // Option to switch on/off checking of volumes overlaps
   //
   G4bool checkOverlaps = true;
@@ -75,21 +82,21 @@ G4VPhysicalVolume* DRUDetectorConstruction::Construct()
   G4double world_sizeZ  = 1.2*env_sizeZ;
   G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
   
-  G4Box* solidWorld =    
+  auto* solidWorld =
     new G4Box("World",                       //its name
        0.5*world_sizeXY, 0.5*world_sizeXY, 0.5*world_sizeZ);     //its size
       
-  G4LogicalVolume* logicWorld =                         
+  auto* logicWorld =
     new G4LogicalVolume(solidWorld,          //its solid
                         world_mat,           //its material
                         "World");            //its name
                                    
   G4VPhysicalVolume* physWorld = 
-    new G4PVPlacement(0,                     //no rotation
+    new G4PVPlacement(nullptr,                     //no rotation
                       G4ThreeVector(),       //at (0,0,0)
                       logicWorld,            //its logical volume
                       "World",               //its name
-                      0,                     //its mother  volume
+                      nullptr,                     //its mother  volume
                       false,                 //no boolean operation
                       0,                     //copy number
                       checkOverlaps);        //overlaps checking
@@ -97,16 +104,16 @@ G4VPhysicalVolume* DRUDetectorConstruction::Construct()
   //     
   // Envelope
   //  
-  G4Box* solidEnv =    
+  auto* solidEnv =
     new G4Box("Envelope",                    //its name
         0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
       
-  G4LogicalVolume* logicEnv =                         
+  auto* logicEnv =
     new G4LogicalVolume(solidEnv,            //its solid
                         env_mat,             //its material
                         "Envelope");         //its name
                
-  new G4PVPlacement(0,                       //no rotation
+  new G4PVPlacement(nullptr,                       //no rotation
                     G4ThreeVector(),         //at (0,0,0)
                     logicEnv,                //its logical volume
                     "Envelope",              //its name
@@ -116,72 +123,107 @@ G4VPhysicalVolume* DRUDetectorConstruction::Construct()
                     checkOverlaps);          //overlaps checking
  
   //     
-  // Shape 1
+  // Donut
   //  
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
+  G4Material* donut_mat = nist->FindOrBuildMaterial("G4_Ge");
+  auto* donut_vis = new G4VisAttributes(blue);
         
-  // Conical section shape       
-  G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
-  G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
-  G4double shape1_hz = 3.*cm;
-  G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
-  G4Cons* solidShape1 =    
-    new G4Cons("Shape1", 
-    shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
-    shape1_phimin, shape1_phimax);
+  // Importing obj/Donut.obj
+  auto donutMesh = CADMesh::TessellatedMesh::FromOBJ("./objs/Donut.obj");
                       
-  G4LogicalVolume* logicShape1 =                         
-    new G4LogicalVolume(solidShape1,         //its solid
-                        shape1_mat,          //its material
-                        "Shape1");           //its name
+  auto* logicDonut =
+    new G4LogicalVolume(donutMesh->GetSolid(),        //its solid
+                        donut_mat,          //its material
+                        "Donut");           //its name
+  logicDonut->SetVisAttributes(donut_vis);
                
-  new G4PVPlacement(0,                       //no rotation
-                    pos1,                    //at position
-                    logicShape1,             //its logical volume
-                    "Shape1",                //its name
+  new G4PVPlacement(nullptr,                       //no rotation
+                    G4ThreeVector(0, 0, 0),  //at position
+                    logicDonut,             //its logical volume
+                    "Donut",                //its name
                     logicEnv,                //its mother  volume
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
 
   //     
-  // Shape 2
+  // Coin
   //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1*cm, 7*cm);
+  G4Material* coin_mat = nist->FindOrBuildMaterial("G4_Si");
+  auto* coin_vis = new G4VisAttributes(green);
 
-  // Trapezoid shape       
-  G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
-  G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
-  G4double shape2_dz  = 6*cm;      
-  G4Trd* solidShape2 =    
-    new G4Trd("Shape2",                      //its name
-              0.5*shape2_dxa, 0.5*shape2_dxb, 
-              0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz); //its size
-                
-  G4LogicalVolume* logicShape2 =                         
-    new G4LogicalVolume(solidShape2,         //its solid
-                        shape2_mat,          //its material
-                        "Shape2");           //its name
+  // Importing obj/Coin.obj
+  auto coinMesh = CADMesh::TessellatedMesh::FromOBJ("./objs/Coin.obj");
+
+  auto* logicCoin =
+    new G4LogicalVolume(coinMesh->GetSolid(),       //its solid
+                        coin_mat,          //its material
+                        "Coin");           //its name
+                        logicCoin->SetVisAttributes(coin_vis);
                
-  new G4PVPlacement(0,                       //no rotation
-                    pos2,                    //at position
-                    logicShape2,             //its logical volume
-                    "Shape2",                //its name
+  new G4PVPlacement(nullptr,                       //no rotation
+                    G4ThreeVector(0, 0, 0),  //at position
+                    logicCoin,               //its logical volume
+                    "Coin",                  //its name
                     logicEnv,                //its mother  volume
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
                 
-  // Set Shape2 as scoring volume
-  //
-  fScoringVolume = logicShape2;
 
+  //     
+  // Inner Housing
   //
+  G4Material* inner_housing_mat = nist->FindOrBuildMaterial("G4_Cu");
+  auto* inner_housing_vis = new G4VisAttributes(red);
+
+  // Importing obj/InnerHousing.obj
+  auto innerHousingMesh = CADMesh::TessellatedMesh::FromOBJ("./objs/InnerHousing.obj");
+                
+  auto* logicInnerHousing =
+    new G4LogicalVolume(innerHousingMesh->GetSolid(),       //its solid
+                        inner_housing_mat,          //its material
+                        "InnerHousing");           //its name
+  logicInnerHousing->SetVisAttributes(inner_housing_vis);
+               
+  new G4PVPlacement(nullptr,                       //no rotation
+                    G4ThreeVector(0, 0, 0),  //at position
+                    logicInnerHousing,               //its logical volume
+                    "InnerHousing",                  //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+
+  //     
+  // Outer Housing
+  //
+  G4Material* outer_housing_mat = nist->FindOrBuildMaterial("G4_Cu");
+  auto* outer_housing_vis = new G4VisAttributes(red);
+
+  // Importing obj/OuterHousing.obj
+  auto outerHousingMesh = CADMesh::TessellatedMesh::FromOBJ("./objs/OuterHousing.obj");
+                
+  auto* logicOuterHousing =
+    new G4LogicalVolume(outerHousingMesh->GetSolid(),       //its solid
+                        outer_housing_mat,          //its material
+                        "OuterHousing");           //its name
+  logicOuterHousing->SetVisAttributes(outer_housing_vis);
+               
+  new G4PVPlacement(nullptr,                       //no rotation
+                    G4ThreeVector(0, 0, 0),  //at position
+                    logicOuterHousing,       //its logical volume
+                    "OuterHousing",          //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+  // Set Coin as scoring volume
+  //
+  fScoringVolume = logicCoin;
+
   //always return the physical World
-  //
   return physWorld;
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
