9af72f469427bbc94c9a4245ce7d8dca73980f80
abhishek-deshmukh <deshmukh.abhishek369@gmail.com>
2021-06-29 10:47:58 +0530

    bug fix: multi file

--- multi.sh
+++ multi.sh
@@ -1,10 +1,10 @@
-./DRU run2.mac output1.root
-./DRU run2.mac output2.root
-./DRU run2.mac output3.root
-./DRU run2.mac output4.root
-./DRU run2.mac output5.root
-./DRU run2.mac output6.root
-./DRU run2.mac output7.root
-./DRU run2.mac output8.root
-./DRU run2.mac output9.root
+./DRU run2.mac output1.root &
+./DRU run2.mac output2.root &
+./DRU run2.mac output3.root &
+./DRU run2.mac output4.root &
+./DRU run2.mac output5.root &
+./DRU run2.mac output6.root &
+./DRU run2.mac output7.root &
+./DRU run2.mac output8.root &
+./DRU run2.mac output9.root &
 ./DRU run2.mac output10.root
